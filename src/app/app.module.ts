4c041f0d38031f4a1cf4fbb12c4e3a698f948a4d
gokceyilmaz <gokceyilmaz.ce@gmail.com>
2021-09-27 22:45:28 +0300

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuardService } from './auth-guard.service';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'mainpage', component: MainPageComponent, canActivate: [AuthGuardService]}, // only accessible if authorised
+      {path: '**', component: LoginComponent}
+    ]),
+    SocialLoginModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true, // keeps the user signed in
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('592058178071-lks40nkp8t8sqaq8rpk9hgleench0sv4.apps.googleusercontent.com') // your client id
+        }
+      ]
+    }
+  },
+    AuthGuardService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
